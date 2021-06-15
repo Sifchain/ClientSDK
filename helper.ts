cab3c4a19aefb5c55e361126f6d927b6db7b205f
rolandnsharp <rolandnsharp@gmail.com>
2021-06-16 08:53:01 +1000

    add isSifAddress and isEthAddress helper functions

--- helper.ts
+++ helper.ts
@@ -2,3 +2,8 @@ exports.isEthAddress = async function (ethAddress) {
     const regex = new RegExp('/^0x[a-fA-F0-9]{40}$/')
     return regex.test(ethAddress)
 }
+
+exports.isSifAddress = async function (ethAddress) {
+    const regex = new RegExp('/^sif[a-zA-Z0-9]{39}$/')
+    return regex.test(ethAddress)
+}
