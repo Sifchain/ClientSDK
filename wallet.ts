9e3feec6d1e79ffe5855592d7468a0a27b5c4cf4
rolandnsharp <rolandnsharp@gmail.com>
2021-07-16 22:06:15 +1000

    use cosmos stargate method of creating wallet

--- wallet.ts
+++ wallet.ts
@@ -1,8 +1,11 @@
 import { Secp256k1HdWallet, coins } from '@cosmjs/launchpad'
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing'
+import { stringToPath } from "@cosmjs/crypto"
 import config from './config'
 import { ethers } from 'ethers'
 
-export const setupWallet = async () => await Secp256k1HdWallet.fromMnemonic(config.mnemonic, config.sifwallet)
+// export const setupWallet = async () => await DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic)
+export const setupWallet = async () => await DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic, config.sifwallet)
 
 // using ethers here instead because Web3 doesn't have any way to generate wallets via mnemonics
 export const ethWallet = ethers.Wallet.fromMnemonic(config.mnemonic)
