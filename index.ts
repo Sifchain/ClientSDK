e764b45696931efe01ff87430c841ba5b427c0a8
nikohosn <nhosn@icloud.com>
2021-09-08 09:52:31 -0700

    add sif wallet for delegate

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { delegate } from './sdk/validators/delegate';
 const start = async () => {
 	const txnStatus = await delegate(
 		'53837',
-		'sifvaloper1d0q6q6afvzk3whu6cy338yth64vau78runp6u8'
+		'sifvaloper16svh7gvexg3p7f2a7l5f8cpj5m7rja4exdjhhq'
 	);
 	//const txnStatus = await delegate(53838, 'sifv')
 	console.log(txnStatus);
