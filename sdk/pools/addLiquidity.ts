bb659e5a9174c14edad2960373f10c76b1b79a9d
rolandnsharp <rolandnsharp@gmail.com>
2021-09-10 18:33:33 +1000

    Update addLiquidity to use proto stargate types

--- sdk/pools/addLiquidity.ts
+++ sdk/pools/addLiquidity.ts
@@ -1,33 +1,56 @@
-import { SigningCosmosClient, coin, coins } from '@cosmjs/launchpad';
+import { StdFee, coin } from '@cosmjs/launchpad';
 import { SigningStargateClient } from '@cosmjs/stargate';
 import { setupWallet, fee, broadcastUrl } from '../../wallet';
+import { MsgAddLiquidity, MsgAddLiquidityResponse } from '../generated/proto/sifnode/clp/v1/tx';
+import { NativeDexClient } from '../client';
+import { Registry } from '@cosmjs/proto-signing';
+
+type MsgAddLiquidityEncodeObject = {
+	typeUrl: string,
+	value: MsgAddLiquidity
+}
+
+type Asset = {
+	symbol: string,
+}
 
 export const addLiquidity = async (
 	externalAsset: string,
 	externalAssetAmount: number,
-	nativeAssetAmount: number,
-	signer: string
+	nativeAssetAmount: number
 ) => {
 	try {
 		const wallet = await setupWallet();
 		const [firstAccount] = await wallet.getAccounts();
+		const signer = firstAccount.address;
 
-		const unsigned_txn = {
-			typeUrl: 'clp/AddLiquidity',
+		const unsigned_txn: MsgAddLiquidityEncodeObject = {
+			typeUrl: '/sifnode.clp.v1.MsgAddLiquidity',
 			value: {
-				external_asset: externalAsset,
-				external_asset_amount: coin(externalAssetAmount, 'rowan'),
-				native_asset_amount: coin(nativeAssetAmount, 'rowan'),
 				signer,
+				externalAsset: { symbol: externalAsset },
+				nativeAssetAmount: `${nativeAssetAmount}`,
+				externalAssetAmount: `${externalAssetAmount}`,
 			},
 		};
 		const client = await SigningStargateClient.connectWithSigner(
 			broadcastUrl,
-			wallet
+			wallet,
+			{
+				registry: new Registry([...NativeDexClient.getGeneratedTypes()])
+			}
 		);
-		// const client = new SigningCosmosClient(broadcastUrl, sender, wallet)
+		const fee: StdFee = {
+			amount: [
+				{
+					denom: 'rowan',
+					amount: '150000',
+				},
+			],
+			gas: '300000',
+		};
 		const txnStatus = await client.signAndBroadcast(
-			wallet.getAccounts()[0].address,
+			firstAccount.address,
 			[unsigned_txn],
 			fee
 		);
---
+++ test/pools/addLiquidity.test.ts
@@ -0,0 +1,10 @@
+import { addLiquidity } from '../../sdk/pools/addLiquidity';
+import config from '../../config';
+
+describe('test addLiquidity feature', () => {
+	it('should fail', async () => {
+        const response = await addLiquidity('ceth', 500, 1000)
+        console.log(response);
+        expect(1).toBe(1)
+	});
+});
