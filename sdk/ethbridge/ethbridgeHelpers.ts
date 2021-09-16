f00fd276ceefdc55856537654098010f76608b06
rolandnsharp <rolandnsharp@gmail.com>
2021-09-16 20:28:15 +1000

    code clean

--- sdk/ethbridge/ethbridgeHelpers.ts
+++ sdk/ethbridge/ethbridgeHelpers.ts
@@ -4,10 +4,12 @@ const ethTokens = require('./ethbridge/assets.sifchain.mainnet.json')
 import Web3 from 'web3'
 const web3 = new Web3(new Web3.providers.HttpProvider(config.ethnode))
 
-export const getWeb3 = function() {
+export const getWeb3 = function () {
   return web3
 }
 
-export const getToken = function (symbol: string) {
-  return ethTokens.find(token => token.symbol === symbol)
+export const getERC20Token = function (symbol: string) {
+  return ethTokens.find(
+    (token) => token.symbol === symbol && token.address.substirng(0, 2) === '0x'
+  )
 }
--- sdk/ethbridge/importToken.ts
+++ sdk/ethbridge/importToken.ts
@@ -1,29 +1,28 @@
 import { setupWallet, ethWallet } from '../../wallet'
 import Web3 from 'web3'
 import config from '../../config'
-import { getToken, getWeb3 } from './ethbridgeHelpers'
+import { getERC20Token, getWeb3 } from './ethbridgeHelpers'
 import { approveSpend } from './approveSpend'
 import { bridgeBank } from './smartContracts/contracts'
 
 const web3 = getWeb3()
 
-// import
+
 export const importToken = async (symbol: string, amount: string) => {
   const sifWallet = await setupWallet('sif')
   const [sifAccount] = await sifWallet.getAccounts()
   const sifAddress = Web3.utils.utf8ToHex(sifAccount.address)
+
   const gas = (await web3.eth.getBlock('latest')).gasLimit // 150000
+  const nonce = (await web3.eth.getTransactionCount(ethWallet.address)) + 1
   let tx
 
-  ////////////////////////
   // ETH -> cETH
-  ////////////////////////
   if (symbol.toLowerCase() === 'eth') {
     // lock
     tx = {
-      nonce: (await web3.eth.getTransactionCount(ethWallet.address)) + 1,
+      nonce,
       to: config.bridgeBankAddress,
-      // from: ethWallet.address,
       value: amount,
       gas,
       data: bridgeBank()
@@ -32,15 +31,13 @@ export const importToken = async (symbol: string, amount: string) => {
     }
   }
 
-  ////////////////////////
   // eRowan -> Rowan
-  ////////////////////////
   if (symbol.toLowerCase() === 'erowan') {
     // approve and burn
     await approveSpend(ethWallet.address, amount, gas)
 
     tx = {
-      nonce: (await web3.eth.getTransactionCount(ethWallet.address)) + 1,
+      nonce,
       to: config.bridgeBankAddress,
       value: 0,
       gas,
@@ -50,21 +47,19 @@ export const importToken = async (symbol: string, amount: string) => {
     }
   }
 
-  ////////////////////////
   // ERC20 -> cToken
-  ////////////////////////
-  const token = getToken(symbol)
-  if (token && symbol !== 'eth') {
+  const erc20Token = getERC20Token(symbol)
+  if (erc20Token && symbol !== 'eth') {
     // approve and lock
     await approveSpend(ethWallet.address, amount, gas)
 
     tx = {
-      nonce: (await web3.eth.getTransactionCount(ethWallet.address)) + 1,
+      nonce,
       to: config.bridgeBankAddress,
       value: amount,
       gas,
       data: bridgeBank()
-        .methods.lock(sifAddress, token.address, amount)
+        .methods.lock(sifAddress, erc20Token.address, amount)
         .encodeABI(),
     }
   }
