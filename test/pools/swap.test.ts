a95e4ab32dcabda9e35ba01322c0f081104d0c6f
rolandnsharp <rolandnsharp@gmail.com>
2021-09-28 07:40:38 +1000

    add swap tests expectation

--- test/pools/swap.test.ts
+++ test/pools/swap.test.ts
@@ -1,11 +1,18 @@
 import { swap } from '../../sdk/pools/swap'
 import config from '../../config'
 // const sifAPI = new PoolsApi(config.apiConfig);
+export const sleep = (ms: number) => new Promise((done) => setTimeout(done, ms))
 
 describe('test swap feature', () => {
-  it('should fail', async () => {
-    const txnStatusSwap = await swap('rowan', 'ceth', '1234', '1')
-    console.log(txnStatusSwap)
-    expect(1).toBe(1)
-  })
+  it('should swap', async () => {
+    const res = await swap('ceth', 'rowan', '120', '1')
+
+    expect(res).toHaveProperty('rawLog')
+    expect(res).toHaveProperty('gasUsed')
+    expect(res).toHaveProperty('transactionHash')
+
+    const successMsg = JSON.parse(res.rawLog)[0].events[1].type
+
+    expect(successMsg).toBe('swap_successful')
+  }, 90000)
 })
