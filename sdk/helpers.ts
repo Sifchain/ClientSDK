ce911832e53c99646ae8d9034365b356ef0487ef
rolandnsharp <rolandnsharp@gmail.com>
2021-09-28 20:26:15 +1000

    add getDexEntryFromSymbol helper

---
+++ sdk/helpers.ts
@@ -0,0 +1,60 @@
+import config from '../config'
+import { NativeDexClient } from './client'
+
+const ethTokens = require('./assets.sifchain.mainnet.json')
+
+import Web3 from 'web3'
+const web3 = new Web3(new Web3.providers.HttpProvider(config.ethnode))
+
+export const getWeb3 = function () {
+  return web3
+}
+
+export const getERC20Token = function (symbol: string) {
+  return ethTokens.find(
+    (token) => token.symbol === symbol && token.homeNetwork === 'ethereum'
+  )
+}
+
+export const getDexEntryFromSymbol = async function (
+  symbol: string,
+  isExportToCosmos: boolean = false
+) {
+  const lowerCaseSymbol = symbol.toLowerCase()
+  // cache dex entries lookup
+  let entries
+  if (!entries) {
+    const dex = await NativeDexClient.connect(config.sifRpc)
+    entries = (await dex.query.tokenregistry.Entries({})).registry.entries
+  } else {
+    console.log('Using cached Dex entries.')
+  }
+
+  if (isExportToCosmos) {
+    return entries.find((entry) => entry.baseDenom === `x${lowerCaseSymbol}`)
+  }
+
+  if (lowerCaseSymbol === 'rowan') {
+    return entries.find((entry) => entry.baseDenom === 'rowan')
+  }
+
+  if (lowerCaseSymbol === 'basecro') {
+    return entries.find((entry) => entry.baseDenom === 'basecro')
+  }
+
+  return entries.find(
+    (entry) =>
+      entry.baseDenom === `c${lowerCaseSymbol}` ||
+      entry.baseDenom === `u${lowerCaseSymbol}`
+  )
+}
+
+export const dexSymbols = async function () {
+  const dex = await NativeDexClient.connect(config.sifRpc)
+  const { entries } = (await dex.query.tokenregistry.Entries({})).registry
+  return entries.map((e) => {
+    if (e.baseDenom === 'rowan') return e.baseDenom
+    if (e.baseDenom === 'basecro') return e.baseDenom
+    return e.baseDenom.substring(1)
+  })
+}
