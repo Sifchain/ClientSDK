e7194b6ce784ca716f2b511667a8ad56513af76d
rolandnsharp <rolandnsharp@gmail.com>
2021-10-27 22:06:16 +1000

    Add env mnemonic check and error message if missing

--- wallet.ts
+++ wallet.ts
@@ -4,11 +4,24 @@ import * as dotenv from 'dotenv'
 dotenv.config({ path: __dirname + '/.env' })
 
 // using ethers here instead because Web3 doesn't have any way to generate wallets via mnemonics
-export const ethWallet = ethers.Wallet.fromMnemonic(process.env.ETH_MNEMONIC)
+const ethMnemonic = process.env.ETH_MNEMONIC
+if (!ethMnemonic) {
+  console.error(
+    'No eth wallet mnemonic found. Add ETH_MNEMONIC string to .env file.'
+  )
+}
+export const ethWallet = ethers.Wallet.fromMnemonic(ethMnemonic)
 
 export const setupWallet = async function (
   prefix: string = 'sif',
   mnemonic: string = process.env.SIF_MNEMONIC
 ) {
-  return await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix })
+  if (!process.env.SIF_MNEMONIC) {
+    console.error(
+      'No sif wallet mnemonic found. Add SIF_MNEMONIC string to .env file.'
+    )
+  }
+  return await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+    prefix,
+  })
 }
